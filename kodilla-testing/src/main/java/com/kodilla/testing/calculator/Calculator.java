cc0aea69116c052d53909d9aa33052208aa5a32d
Jaroslaw Warowny <jwarowny99@gmail.com>
2021-12-11 00:05:15 +0100

    Czym jest Test Driven Developmen

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public void simpleCalculator() {
+
+        int a = 9;
+        int b = 5;
+        int sum = a + b;
+        int sub = a - b;
+        System.out.println(sum);
+        System.out.println(sub);
+    }
+}
