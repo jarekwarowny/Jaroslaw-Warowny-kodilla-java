7172c98318046952dd6ea70087d2244e1ab7c52e
Jaroslaw Warowny <jwarowny99@gmail.com>
2021-12-17 01:38:18 +0100

    6.5 rozbudowa WeatherForecastTestSuite

--- kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
@@ -1,7 +1,10 @@
 package com.kodilla.testing.weather.stub;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class WeatherForecast {
 
@@ -16,9 +19,42 @@ public class WeatherForecast {
 
         for (Map.Entry<String, Double> temperature :
                 temperatures.getTemperatures().entrySet()) {
-
             resultMap.put(temperature.getKey(), temperature.getValue() + 1.0); // [1]
         }
         return resultMap;
     }
+
+    public Double calculateAverage() {
+        Map<String, Double> theFirstMap = new HashMap<>();
+
+        for (Map.Entry<String, Double> temperature :
+                temperatures.getTemperatures().entrySet()) {
+            theFirstMap.put(temperature.getKey(), temperature.getValue() + 0.0);
+        }
+
+        double sumOfAverage = theFirstMap.values().stream().mapToDouble(Double::doubleValue).average().orElse(0);
+        Map.Entry<String, Double> e = theFirstMap.entrySet().stream().min(Comparator.comparing(a -> Math.abs(a.getValue() - sumOfAverage))).orElse(null);
+        return sumOfAverage;
+    }
+    public Double calculateMedian() {
+        Map<String, Double> theSecondMap = new HashMap<>();
+
+        for (Map.Entry<String, Double> temperature :
+                temperatures.getTemperatures().entrySet()) {
+            theSecondMap.put(temperature.getKey(), temperature.getValue() + 0.0);
+        }
+        Object[] values = theSecondMap.values().toArray();
+        Arrays.sort(values);
+        double sumOfMedian = 0;
+        int totalElements = theSecondMap.size();
+        int sumOfMiddleElements;
+        if (totalElements % 2 == 0) {
+            sumOfMiddleElements = (int) values[totalElements / 2 + totalElements / 2 - 1];
+            sumOfMedian = (double) sumOfMiddleElements / 2;
+        } else {
+            sumOfMedian = (double) values[values.length / 2];
+        }
+        return sumOfMedian;
+    }
 }
+
--- kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
@@ -4,31 +4,71 @@ import com.kodilla.testing.weather.stub.Temperatures;
 import com.kodilla.testing.weather.stub.WeatherForecast;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class WeatherForecastTestSuite {
+
+    @Mock
+    private Temperatures temperaturesMock;
 
     @Test
     void testCalculateForecastWithMock() {
         //Given
-        Temperatures temperaturesMock = mock(Temperatures.class);
         Map<String, Double> temperaturesMap = new HashMap<>();                     // [14]
         temperaturesMap.put("Rzeszow", 25.5);                                      // [15]
         temperaturesMap.put("Krakow", 26.2);                                       // [16]
         temperaturesMap.put("Wroclaw", 24.8);                                      // [17]
         temperaturesMap.put("Warszawa", 25.2);                                     // [18]
         temperaturesMap.put("Gdansk", 26.1);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
         WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
-
         //When
         int quantityOfSensors = weatherForecast.calculateForecast().size();
-
         //Then
         Assertions.assertEquals(5, quantityOfSensors);
+    }
+
+    @Test
+    void testCalculateAverage() {
+        //Given
+        Map<String, Double> temperaturesMap = new HashMap<>();                     // [14]
+        temperaturesMap.put("Rzeszow", 25.5);                                      // [15]
+        temperaturesMap.put("Krakow", 26.2);                                       // [16]
+        temperaturesMap.put("Wroclaw", 24.8);                                      // [17]
+        temperaturesMap.put("Warszawa", 25.2);                                     // [18]
+        temperaturesMap.put("Gdansk", 26.1);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+        //When
+        Double testOfCalculateAverage = weatherForecast.calculateAverage();
+        //Then
+        Assertions.assertEquals(25.56, testOfCalculateAverage);
+    }
+
+    @Test
+    void testCalculateMedian() {
+        //Given
+        Map<String, Double> temperaturesMap = new HashMap<>();                     // [14]
+        temperaturesMap.put("Rzeszow", 25.5);                                      // [15]
+        temperaturesMap.put("Krakow", 26.2);                                       // [16]
+        temperaturesMap.put("Wroclaw", 24.8);                                      // [17]
+        temperaturesMap.put("Warszawa", 25.2);                                     // [18]
+        temperaturesMap.put("Gdansk", 26.1);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+        //When
+        Double testOfCalculateMedian = weatherForecast.calculateMedian();
+        //Then
+        Assertions.assertEquals(25.5, testOfCalculateMedian);
     }
 }
 
