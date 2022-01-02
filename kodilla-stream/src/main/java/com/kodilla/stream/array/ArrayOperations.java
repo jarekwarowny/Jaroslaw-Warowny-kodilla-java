c50c4c374a77b970eec576ec178f7e375a2d044d
Jaroslaw Warowny <jwarowny99@gmail.com>
2022-01-03 00:51:00 +0100

    7.5 obliczamy srednia funkcyjnie

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,5 +1,17 @@
 package com.kodilla.stream.array;
 
-public interface ArrayOperations {
+import java.util.stream.IntStream;
 
+public interface ArrayOperations {
+    static double getAverage(int[] numbers) {
+
+        IntStream.range(0, numbers.length)
+                .map(n -> numbers[n]);
+
+         double sum = IntStream.range(0, numbers.length)
+                     .map(n -> numbers[n])
+                     .average()
+                     .getAsDouble();
+         return sum;
+    }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,5 +1,15 @@
 package com.kodilla.stream.array;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ArrayOperationsTestSuite {
 
+    @Test
+    void testGetAverage() {
+        int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        double average = ArrayOperations.getAverage(numbers);
+        Assertions.assertEquals(10.5, average, 1);
+        System.out.println(average);
+    }
 }
