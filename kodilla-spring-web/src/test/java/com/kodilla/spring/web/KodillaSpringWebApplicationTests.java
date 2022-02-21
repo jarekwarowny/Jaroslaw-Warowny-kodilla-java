1a0941c37310a39d22c0c96ddf42ff9f9a223968
Jarek Warowny <jwarowny99@gmail.com>
2022-02-22 00:02:45 +0100

    14.4 wszystko w jednym

---
+++ kodilla-spring-web/src/test/java/com/kodilla/spring/web/KodillaSpringWebApplicationTests.java
@@ -0,0 +1,14 @@
+package com.kodilla.spring.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+public class KodillaSpringWebApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaSpringWebApplication.class, args);
+	}
+}
