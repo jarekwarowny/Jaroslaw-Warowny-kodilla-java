eb83a58cc7d9a677e3b09fb31840665ed08cded4
Jarek Warowny <jwarowny99@gmail.com>
2022-02-22 00:09:25 +0100

    14.4 wszystko w jednym

--- kodilla-spring-web/src/main/java/com/kodilla/spring/web/KodillaSpringWebApplication.java
+++ kodilla-spring-web/src/main/java/com/kodilla/spring/web/KodillaSpringWebApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KodillaSpringWebApplication {
+class KodillaSpringWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KodillaSpringWebApplication.class, args);
 	}
-
 }
--- kodilla-spring-web/src/test/java/com/kodilla/spring/web/KodillaSpringWebApplicationTests.java
+++ kodilla-spring-web/src/test/java/com/kodilla/spring/web/KodillaSpringWebApplicationTests.java
@@ -1,14 +1,13 @@
 package com.kodilla.spring.web;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
+@SpringBootTest
 public class KodillaSpringWebApplicationTests {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KodillaSpringWebApplication.class, args);
+	@Test
+	void contextLoads() {
 	}
+
 }
