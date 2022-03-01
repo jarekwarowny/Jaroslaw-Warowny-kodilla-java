78db05c702e29be3295c993ba5c043322ea0b013
Jarek Warowny <jwarowny99@gmail.com>
2022-03-01 21:21:41 +0100

    15.5 poprawione

--- kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Bigmac.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Bigmac.java
@@ -41,7 +41,7 @@ public final class Bigmac {
         }
     }
 
-    public Bigmac(final String bun, final int burgers, final String sauce, List<String> ingredients) {
+    private Bigmac(final String bun, final int burgers, final String sauce, List<String> ingredients) {
         this.bun = bun;
         this.burgers = burgers;
         this.sauce = sauce;
